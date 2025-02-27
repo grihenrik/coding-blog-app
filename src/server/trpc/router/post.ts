572c501a0f16266dedf4e2741e187494e3201945
Henrik Gripenberg <henrik@pinesoft.fi>
2025-02-27 14:31:03 +0200

    Updated the post router

--- src/server/trpc/router/post.ts
+++ src/server/trpc/router/post.ts
@@ -1,6 +1,7 @@
 import { router, protectedProcedure, publicProcedure } from "../trpc";
 import slugify from "slugify";
 import { writeFormSchema } from "../../../components/WriteFormModal";
+import { z } from "zod";
 
 export const postRouter = router({
   createPost: protectedProcedure
@@ -8,7 +9,7 @@ export const postRouter = router({
     .mutation(
       async ({
         ctx: { prisma, session },
-        input: { title, description, text },
+        input: { title, description, text, html },
       }) => {
         // check post already exist
         const post = await prisma?.post.findFirst({
@@ -23,7 +24,8 @@ export const postRouter = router({
           data: {
             title: title,
             description: description,
-            text: text,
+            text: text === undefined ? "" : text,
+            html: html === undefined ? "" : html,
             slug: slugify(title),
             author: {
               connect: {
@@ -34,7 +36,145 @@ export const postRouter = router({
         });
       }
     ),
-  read: publicProcedure.query(async ({ ctx: { prisma } }) => {
-    return await prisma?.post.findMany();
+  read: publicProcedure.query(async ({ ctx: { prisma, session } }) => {
+    return await prisma?.post.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      select: {
+        id: true,
+        title: true,
+        description: true,
+        featuredImage: true,
+        createdAt: true,
+        slug: true,
+        author: {
+          select: {
+            name: true,
+            image: true,
+          },
+        },
+        bookmarks: session?.user?.id
+          ? {
+              where: {
+                userId: session?.user?.id ?? "",
+              },
+            }
+          : false,
+      },
+    });
   }),
+  readOne: publicProcedure
+    .input(z.object({ slug: z.string() }))
+    .query(async ({ ctx: { prisma, session }, input: { slug } }) => {
+      return await prisma?.post.findUnique({
+        where: {
+          slug: slug,
+        },
+        select: {
+          id: true,
+          title: true,
+          description: true,
+          text: true,
+          featuredImage: true,
+          likes: session?.user?.id
+            ? {
+                where: {
+                  userId: session?.user?.id,
+                },
+              }
+            : false,
+        },
+      });
+    }),
+  likePost: protectedProcedure
+    .input(z.object({ postId: z.string() }))
+    .mutation(async ({ ctx: { prisma, session }, input: { postId } }) => {
+      const like = await prisma?.like.findFirst({
+        where: {
+          userId: session.user.id,
+          postId: postId,
+        },
+      });
+      if (like !== null) {
+        await prisma?.like.delete({
+          where: {
+            id: like.id,
+          },
+        });
+        return;
+      }
+      await prisma?.like.create({
+        data: {
+          user: {
+            connect: {
+              id: session.user.id,
+            },
+          },
+          post: {
+            connect: {
+              id: postId,
+            },
+          },
+        },
+      });
+    }),
+  getLikes: publicProcedure
+    .input(z.object({ postId: z.string() }))
+    .query(async ({ ctx: { prisma }, input: { postId } }) => {
+      return await prisma?.like.count({
+        where: {
+          postId: postId,
+        },
+      });
+    }),
+
+  deletePost: protectedProcedure
+    .input(z.object({ postId: z.string() }))
+    .mutation(async ({ ctx: { prisma }, input: { postId } }) => {
+      await prisma?.post.delete({
+        where: {
+          id: postId,
+        },
+      });
+    }),
+  updatePost: protectedProcedure
+    .input(z.object({ postId: z.string(), data: writeFormSchema }))
+    .mutation(async ({ ctx: { prisma }, input: { postId, data } }) => {
+      await prisma?.post.update({
+        where: {
+          id: postId,
+        },
+        data: {
+          title: data.title,
+          description: data.description,
+          text: data.text,
+          html: data.html,
+        },
+      });
+    }),
+  bookmarkPost: protectedProcedure
+    .input(z.object({ postId: z.string() }))
+    .mutation(async ({ ctx: { prisma, session }, input: { postId } }) => {
+      const bookmark = await prisma?.bookmark.findFirst({
+        where: {
+          userId: session.user.id,
+          postId: postId,
+        },
+      });
+      if (bookmark !== null) {
+        await prisma?.bookmark.delete({
+          where: {
+            id: bookmark.id,
+          },
+        });
+        return;
+      }
+      await prisma?.bookmark.create({
+        data: {
+          userId: session.user.id,
+          postId: postId,
+        },
+      });
+    }),
 });
