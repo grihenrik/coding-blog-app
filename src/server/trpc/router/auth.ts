45a6318b9284d38cfbb5a26a2f7c322c3391a054
Henrik Gripenberg <henrik@pinesoft.fi>
2025-02-27 14:31:26 +0200

    Improved the session handling in auth

--- src/server/trpc/router/auth.ts
+++ src/server/trpc/router/auth.ts
@@ -1,7 +1,15 @@
+import { useContext } from "react";
 import { router, publicProcedure, protectedProcedure } from "../trpc";
+import { GlobalContext } from "../../../contexts/GlobalContextProvider";
 
 export const authRouter = router({
   getSession: publicProcedure.query(({ ctx }) => {
+    const { session } = ctx;
+    if (!session) {
+      throw new Error("No session found");
+    }
+    const { userId, setUserId } = useContext(GlobalContext);
+    if (!userId && session.user) setUserId(session.user.id);
     return ctx.session;
   }),
   getSecretMessage: protectedProcedure.query(() => {
